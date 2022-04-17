bcdde8572d7f4ea79b88ccff4059b608e56db3dc
RaimundoJSoares <raianimes1000@gmail.com>
2022-04-17 16:26:28 -0400

    adicionando métodos e criando página saber mais

--- learninglaravel/resources/views/welcome.blade.php
+++ learninglaravel/resources/views/welcome.blade.php
@@ -1,38 +1,34 @@
 @extends('layouts.main')
 
-@section('title', 'HDC events')
+@section('title', 'HDC Events')
 
 @section('content')
 
 <div id="search-container" class="col-md-12">
     <h1>Busque um evento</h1>
     <form action="">
-        <input type="text" id='search' name="search" class="form-control" placeholder="procurar...">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
     </form>
 </div>
- <div id="events-container" class="cold-md-12">
-     <h2>Próximos Eventos</h2>
-     <p>Veja os eventos nos próximos dias</p>
-
-     <div id="cards-container" class="row">
+<div id="events-container" class="col-md-12">
+    <h2>Próximos Eventos</h2>
+    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
         @foreach($events as $event)
-            <div id="card" class="col-md-3">
-                <img src="/img/logo0.jpg" alt="{{ $event->title }}">
-                <div class="card-body">
-                    <p class="card-date">
-                        01/05/2022
-                    </p>
-                    <h5 class="card-title">
-                        {{ $event->title }}
-                    </h5>
-                    <p class="card-participants">
-                        X participantes
-                    </p>
-                    <a href="#" class="btn-btn-primary">Saber Mais</a>
-                </div>
+        <div class="card col-md-3">
+            <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
+            <div class="card-body">
+                <p class="card-date">{{ date('d/m/Y', strtotime($event->date)) }}</p>
+                <h5 class="card-title">{{ $event->title }}</h5>
+                <p class="card-participants">X Participantes</p>
+                <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
             </div>
+        </div>
         @endforeach
-     </div>
- </div>
+        @if(count($events) == 0)
+            <p>Não há eventos disponíveis</p>
+        @endif
+    </div>
+</div>
 
 @endsection
